36be870ac37f0f8156a964d26ab81ebaab95e1fc
yzhou <yzhou@buaa.edu.cn>
2017-08-14 13:45:35 +0800

    FPS UIView+Frame

---
+++ YYFPSLabel.h
@@ -0,0 +1,13 @@
+//
+//  YYFPSLabel.h
+//  patient-app
+//
+//  Created by apple on 2017/8/6.
+//  Copyright © 2017年 zhuhongji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYFPSLabel : UILabel
+
+@end
